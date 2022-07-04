c441b193c1c11218a7bf3c2e5ec237d7a6b964a5
DaisyKoome <dskoome@gmail.com>
2022-07-04 18:10:16 +0300

    Finished basics on routing and creating an api

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello World<h1>', 200) 
-        ->header('Content-Type','text/plain');
+
+Route::get('/hello', function() {
+    return response('<h1>Hello world</h1>')
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
 });
 
 Route::get('/posts/{id}', function($id){
+    ddd($id);
     return response('Post '.$id);
-})->where('id','[0-9]+');
+})->where('id', '[0-9]+');
 
 Route::get('/search', function(Request $request){
     return $request->name . ' ' . $request->city;
