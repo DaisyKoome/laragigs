4a8188c376a25a8191aa712466eb6deebda83f12
DaisyKoome <dskoome@gmail.com>
2022-07-09 17:21:21 +0300

    Unless directive
    Unless directive

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+
+        ]
+    ]);
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello World<h1>', 200) 
-        ->header('Content-Type','text/plain');
-});
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World<h1>', 200) 
+//         ->header('Content-Type','text/plain');
+// });
 
-Route::get('/posts/{id}', function($id){
-    return response('Post '.$id);
-})->where('id','[0-9]+');
+// Route::get('/posts/{id}', function($id){
+//     return response('Post '.$id);
+// })->where('id','[0-9]+');
 
-Route::get('/search', function(Request $request){
-    return $request->name . ' ' . $request->city;
-});
+// Route::get('/search', function(Request $request){
+//     return $request->name . ' ' . $request->city;
+// });
