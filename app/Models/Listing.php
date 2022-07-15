08f034cb8a939703c83ea41965574a85bc499224
DaisyKoome <dskoome@gmail.com>
2022-07-15 15:37:22 +0300

    Database migration, eloquent model, adding theme html
    Database migration, eloquent model, adding theme html

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,40 +1,11 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    class Listing {
-        public static function all(){
-            return [
-                    [
-                        'id' => 1,
-                        'title' => 'Listing One',
-                        'description' => 'Lorem ipsum dolor sit amet, 
-                        consectetur adipiscing elit. Aliquam rhoncus ante ac lectus tempor, 
-                        eu sollicitudin lacus feugiat. Nullam et laoreet purus. 
-                        Donec maximus augue sed justo suscipit, 
-                        a condimentum tellus tincidunt.'
-                    ],
-        
-                    [
-                        'id' => 2,
-                        'title' => 'Listing Two',
-                        'description' => 'Sed auctor pulvinar ex. Aliquam mi sapien, 
-                        commodo eu nunc et, viverra lobortis nunc. 
-                        Pellentesque dui eros, accumsan et euismod eu, 
-                        imperdiet vitae enim. Vivamus sed nisi non nisl hendrerit vulputate.'
-                    ]
-                ];
-        }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-        public static function find($id){
-            $listings = self::all();
-
-            foreach($listings as $listing){
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-        }
-    }
-
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(5)->create();
+
+         Listing::factory(6)->create();
+
+        //  Listing::create([
+        //     'title'=>'Senior Laravel Developer',
+        //     'tags'=>'Laravel, JavaScript',
+        //     'company'=>'Belva',
+        //     'location'=>'Nairobi',
+        //     'email'=>'people@belvadigital.com',
+        //     'website'=>'belvadigital.com',
+        //     'description'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        //     Nullam suscipit vel odio ac tempus. Fusce consequat magna sit amet nisl consectetur cursus. 
+        //     Fusce euismod a risus consequat varius.'
+        //  ]);
+
+        //  Listing::create([
+        //     'title'=>'Junior React Developer',
+        //     'tags'=>'React, Python',
+        //     'company'=>'Belva',
+        //     'location'=>'Nairobi',
+        //     'email'=>'people@belvadigital.com',
+        //     'website'=>'belvadigital.com',
+        //     'description'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        //     Nullam suscipit vel odio ac tempus. Fusce consequat magna sit amet nisl consectetur cursus. 
+        //     Fusce euismod a risus consequat varius.'
+        //  ]);
+
     }
 }
