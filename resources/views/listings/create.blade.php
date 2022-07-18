365c23a54da8aee3cc72344b9fd9278797e71bf7
DaisyKoome <dskoome@gmail.com>
2022-07-18 12:27:13 +0300

    Value persistence in the input fields on error
    Value persistence in the input fields on error

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
         <input
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
-            name="company"
+            name="company" value="{{ old('company') }}"
         />
 
         @error('company')
@@ -35,7 +35,8 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="title"
-            placeholder="Example: Senior Laravel Developer"
+            placeholder="Example: Senior Laravel Developer" 
+            value="{{ old('title') }}"
         />
 
         @error('title')
@@ -55,6 +56,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
+            value="{{ old('location') }}"
         />
 
         @error('location')
@@ -71,6 +73,7 @@
             type="email"
             class="border border-gray-200 rounded p-2 w-full"
             name="email"
+            value="{{ old('email') }}"
         />
 
         @error('email')
@@ -90,6 +93,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
+            value="{{ old('website') }}"
         />
 
         @error('website')
@@ -107,6 +111,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
+            value="{{ old('tags') }}"
         />
         @error('tags')
         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -136,7 +141,8 @@
             name="description"
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
-        ></textarea>
+            
+        >{{ old('description') }}</textarea>
 
         @error('description')
             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
