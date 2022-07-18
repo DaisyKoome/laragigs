4cc5c9bdab6ce16cacf5eb111caf08f58bff03aa
DaisyKoome <dskoome@gmail.com>
2022-07-18 12:51:01 +0300

    From 2 to 6 per page
    From 2 to 6 per page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
     public function index(){
         return view('listings.index', [
             'listings' => Listing::latest()->filter(request(['tag',
-            'search']))->paginate(2)
+            'search']))->paginate(6)
         ]);
     }
 
