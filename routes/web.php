ae1ade2375ad5cdb0254174fabbd25b682c55b4b
DaisyKoome <dskoome@gmail.com>
2022-07-18 17:18:49 +0300

    Update/edit functionality done
    Update/edit functionality done

--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,17 @@ use phpDocumentor\Reflection\PseudoTypes\List_;
 //All Listings
 Route::get('/', [ListingController::class, 'index']);
 
-//Show create form
+//Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
 //Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show Edit Form
+Route::get('listings/{listing}/edit', [ListingController::class, 'edit']);
 
-
+//Update Listings
+Route::put('listings/{listing}/', [ListingController::class, 'update']);
 
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
